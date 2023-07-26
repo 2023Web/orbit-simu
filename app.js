f7826bf1080b44883d28cda4a79395ad7e8e8373
nekko <KingSann@foxmail.com>
2023-07-26 19:32:21 +0800

    fix: 修改了planet api的部分问题

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var planet_hdl = require('./planet.js')
 const path = require('path');
 var app = express();
 
-app.use('/static',express.static(path.join(__dirname, 'public')));
+app.use('/static',express.static(path.join(__dirname, 'static')));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/" + "index.html");
 });
--- planet.js
+++ planet.js
@@ -29,10 +29,74 @@ class Planet {
 
 // TODO: 需要添加判断日食月食的代码
 
+// FIXME: 部分参数可能有误，请检查
+var planet_coef = {
+  solar: {
+    radius: 0.0046491, // AU
+    speed: 14.713, // degree/day
+    angle: 0,
+    clockwise: 1,
+    orbit_radius: 0, // 太阳可以视为固定在原点
+    orbit_speed: 0,
+    orbit_angle: 0,
+    orbit_clockwise: 1,
+  },
+  earth: {
+    radius: 0.000042634, // AU
+    speed: 0.0041781, // degree/s
+    angle: 0,
+    clockwise: 1, // 北极上空俯视呈逆时针方向旋转
+    orbit_radius: 1, // AU
+    orbit_speed: 0.986, // degree/day
+    orbit_angle: 0,
+    orbit_clockwise: 1,
+  },
+  lunar: {
+    radius: 0.000011264, // AU
+    speed: 0.0041781, // degree/s
+    angle: 0,
+    clockwise: 1, // 北极上空俯视呈逆时针方向旋转
+    orbit_radius: 0.00257, // AU
+    orbit_speed: 13.176358, // degree/day
+    orbit_angle: 0,
+    orbit_clockwise: 1,
+  },
+};
+
+var time_coef = 10 / 86400; // 1s = 10day
 
 module.exports = {
-  // FIXME: 为了方便测试，这里的参数都是乱写的
-  solar: new Planet(50, 0.01, 0, 1, 0, 0, 0, 1, 1),
-  earth: new Planet(10, 0.02, 0, 1, 200, 0.01, 0, 1, 1),
-  lunar: new Planet(5, 0.03, 0, 1, 50, 0.02, 0, 1, 1),
+  solar: new Planet(
+    planet_coef.solar.radius,
+    planet_coef.solar.speed,
+    planet_coef.solar.angle,
+    planet_coef.solar.clockwise,
+    planet_coef.solar.orbit_radius,
+    planet_coef.solar.orbit_speed,
+    planet_coef.solar.orbit_angle,
+    planet_coef.solar.orbit_clockwise,
+    time_coef,
+  ),
+  earth: new Planet(
+    planet_coef.earth.radius,
+    planet_coef.earth.speed,
+    planet_coef.earth.angle,
+    planet_coef.earth.clockwise,
+    planet_coef.earth.orbit_radius,
+    planet_coef.earth.orbit_speed,
+    planet_coef.earth.orbit_angle,
+    planet_coef.earth.orbit_clockwise,
+    time_coef,
+  ),
+  lunar: new Planet(
+    planet_coef.lunar.radius,
+    planet_coef.lunar.speed,
+    planet_coef.lunar.angle,
+    planet_coef.lunar.clockwise,
+    planet_coef.lunar.orbit_radius,
+    planet_coef.lunar.orbit_speed,
+    planet_coef.lunar.orbit_angle,
+    planet_coef.lunar.orbit_clockwise,
+    time_coef,
+  ),
 };
